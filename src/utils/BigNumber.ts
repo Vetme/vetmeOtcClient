b95623009a59a77af2c741e4b245767f794a44ff
makerzy <botassammzy@gmail.com>
2022-12-28 15:06:06 -0500

    some debugging

--- src/utils/BigNumber.ts
+++ src/utils/BigNumber.ts
@@ -4,7 +4,7 @@ export const BIG_ZERO = new BigNumber("0");
 export const BIG_ONE = new BigNumber("1");
 export const BIG_NINE = new BigNumber("9");
 export const BIG_TEN = new BigNumber("10");
-export const toBN = (val: any) => new BigNumber(val);
+export const toBN = (val: number | string) => new BigNumber(val);
 export const toDefaultDecimal = (val: any) => toBN(val).times(BIG_TEN.pow(18));
 export const toDynamicDecimal = (val: any, pow: any) =>
   toBN(val).times(BIG_TEN.pow(pow));
